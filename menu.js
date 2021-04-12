030c754045195ceff5b9509a0c4807ebb7d5940c
guilherme <78052265+Guilherme275149@users.noreply.github.com>
2021-04-11 23:52:24 -0300

    Add files via upload

---
+++ menu.js
@@ -0,0 +1,136 @@
+const help = (p, date, user, wame) => {
+return `
+
+╔╦══• •✠•❀•✠ • •══╦╗
+║ comandos
+║
+║ 𝐁𝐎𝐓 : 「 𝙀𝙡𝙞𝙯𝙖𝙗𝙚𝙩𝙝 𝘽𝙊𝙏 」
+║ 𝐏𝐑𝐄𝐅𝐈𝐗𝐎 : /
+║ 𝐃𝐎𝐍𝐎 : wa.me/+5575998868032
+║
+╚╩══• •✠•❀•✠ • •══╩╝
+
+
+┏  「 Menu Principal 」
+╟➤listblockcmd
+╟➤listpalavra
+╟➤info
+╟➤ping
+╟➤destrava
+╟➤sugerircmd ❮text❯
+╟➤blocklist
+╟➤blacklist
+╟➤criador
+╟➤toimg ❮caption, reply img❯
+╽
+╽┷╼❥
+
+
+┏  「 Menu Fig 」
+╟➤sticker ❮caption, reply img❯
+╟➤sticker ❮caption, reply img❯
+╟➤fstiker ❮caption, reply img❯
+╟➤fsticker ❮caption, reply img❯
+╟➤attp ❮txt❯
+╟➤macaco
+╽
+╽┷╼❥
+
+
+┏ 「 Menu Video 」
+╟➤frame ❮sec❯ ❮reply video❯
+╟➤mute ❮reply video❯
+╟➤rapidovid ❮reply video❯
+╟➤lentovid ❮reply video❯
+╟➤togif ❮reply video❯
+╟➤resizevid ❮lag❯ ❮alt❯ ❮reply vid❯
+╽┷╼❥
+
+
+┏  「 Menu Pesquisa 」
+╟➤saycat
+╟➤biblia
+╟➤animesrc ❮name anime❯
+╽┷╼❥
+
+
+┏  「 Menu Estourado 」
+╟➤esquilo ❮reply audio❯
+╟➤grave ❮reply audio❯
+╟➤estourar ❮reply audio❯
+╟➤rápido audio ❮reply audio❯
+╟➤lentoaudio ❮reply audio❯
+╟➤bass ❮reply audio❯
+╽┷╼❥
+
+
+┏ 「 Menu Música」
+╟➤play ❮music name❯
+╟➤playv2 ❮music name❯
+╽┷╼❥
+
+
+┏ 「 Menu Antis 」
+╟➤antidoc ❮1 or 0❯
+╟➤antiloc ❮1 or 0❯
+╟➤antiimg ❮1 or 0❯
+╟➤antipalavra ❮1 or 0❯
+╟➤antivideo ❮1 or 0❯
+╟➤antisticker ❮1 or 0❯
+╟➤antiaudio ❮1 or 0❯
+╟➤antictt ❮1 or 0❯
+╟➤antilink ❮1 or 0❯
+╟➤antiporn ❮1 or 0❯
+╟➤antifake ❮1 or 0❯
+╟➤antilinkhard ❮1 or 0❯
+╽┷╼❥
+
+
+┏ 「 Menu Grupo 」
+╟➤fechargp
+╟➤mudardesc
+╟➤mudarnome
+╟➤abrirgp
+╟➤linkgroup
+╟➤listadmin
+╟➤listonline
+╟➤leave
+╟➤kick ❮dial num❯
+╟➤ban ❮reply message❯
+╟➤promote ❮dial num❯
+╟➤demote ❮dial num❯
+╟➤add ❮num❯
+╟➤roletrussablock ❮1 or 0❯
+╟➤welcome ❮1 or 0❯
+╟➤simih ❮1 or 0❯
+╟➤autostickerimg ❮1 or 0❯
+╟➤onmodgrupo
+╟➤offmodgrupo
+╟➤autoreply ❮1 or 0❯
+╟➤marcar
+╟➤marcar2
+╟➤marcar3
+╟➤hidemarcar ❮txt❯
+╽┷╼❥
+
+
+┏ 「 Menu Criador 」
+╟➤blockcmd ❮cmd less prefix❯
+╟➤unblockcmd ❮cmd less prefix❯
+╟➤alerta ❮txt❯
+╟➤infinityblock ❮1 or 0❯
+╟➤addpalavra ❮txt❯
+╟➤removepalavra ❮txt❯
+╟➤limparchat ❮txt❯
+╟➤ataque ❮txt❯
+╟➤divulgar ❮txt❯
+╟➤entrargp ❮txt❯
+╟➤block ❮num❯
+╟➤unblock ❮num❯
+╽┷╼❥`
+}
+
+exports.help = help
+
+
+
---
+++ welcomtext.js
@@ -0,0 +1,9 @@
+const welcome = (number, groupname) => {
+    return `𝐎𝐥𝐚 @${number}. 𝐒𝐞𝐣𝐚 𝐛𝐞𝐦-𝐯𝐢𝐧𝐝𝐨 𝐚𝐨 𝐠𝐫𝐮𝐩𝐨 𝐋𝐞𝐢𝐚 𝐚𝐬 𝐫𝐞𝐠𝐫𝐚𝐬 𝐧𝐚 𝐝𝐞𝐬𝐜 𝐩𝐚𝐫𝐚 𝐧𝐚̃𝐨 𝐬𝐞𝐫 𝐫𝐞𝐦𝐨𝐯𝐢𝐝𝐨${groupname}`
+}
+exports.welcome = welcome
+
+const bye = (number) => {
+    return `Adeus @${number}. 👋`
+}
+exports.bye = bye
